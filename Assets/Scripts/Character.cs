a9cc0e6bea79a4b6ead069f0adceb6c2cd82b168
Halim Can Başkaya <cnbskya@gmail.com>
2021-04-13 16:06:44 +0300

    Arena Added

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -38,7 +38,6 @@ public class Character : MonoBehaviour
 		if (other.gameObject.CompareTag("GroundObstacle"))
 		{
 			animator.SetBool("groundReact", true);
-			Debug.Log("ground");
 			//animator.SetBool
 		}
 		else if (other.gameObject.CompareTag("AirObstacle"))
